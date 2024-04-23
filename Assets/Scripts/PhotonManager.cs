b517f722b8f970fe75d58e32f8ad65adb554cfa5
Shah Rukh Abdullah <samansaeed2306@gmail.com>
2024-04-24 02:55:37 +0500

    Room Creation Working

--- Assets/Scripts/PhotonManager.cs
+++ Assets/Scripts/PhotonManager.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using TMPro;
-
+using Photon.Realtime;
 public class PhotonManager : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     public TMP_InputField userNameText;
+    public TMP_InputField roomNameText;
 
+    public TMP_InputField maxPlayer;
     public GameObject PlayerNamePanel;
     public GameObject LobbyPanel;
     public GameObject RoomCreatePanel;
     public GameObject LoadingPanel;
+
     #region UnityMethods
 
     void Start()
@@ -41,8 +44,53 @@ public class PhotonManager : MonoBehaviourPunCallbacks
         {
             Debug.Log("Empty name ");
         }
+
+    }
+    public void OnClickCreateRoom()
+    {
+        string roomName = roomNameText.text.Trim();
+        if (string.IsNullOrEmpty(roomName))
+        {
+            roomName = "Room" + Random.Range(0, 1000);
+        }
+
+        int maxPlayers;
+        if (!int.TryParse(maxPlayer.text, out maxPlayers))
+        {
+            Debug.Log("Invalid Max Players value");
+            return;
+        }
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = (byte)maxPlayers;
+
+        PhotonNetwork.CreateRoom(roomName, roomOptions);
+
+        Debug.Log("Room Name: " + roomName);
+        Debug.Log("Max Players: " + maxPlayers);
+    }
+
+    // public void OnClickCreateRoom()
+    // {
+    //     string roomName = roomNameText.text;
+    //     if (string.IsNullOrEmpty(roomName))
+    //     {
+    //         roomName = roomName + Random.Range(0, 1000);
+    //         RoomOptions roomOptions = new RoomOptions();
+    //         roomOptions.MaxPlayers = (byte)int.Parse(maxPlayer.text);
+    //         PhotonNetwork.CreateRoom(roomName, roomOptions);
+    //         Debug.Log("Room Name: " + roomNameText.text);
+    //         Debug.Log("Max Players: " + maxPlayer.text);
+
+    //     }
+    // }
+    public void OnCancelClick()
+    {
+        Debug.Log("You have exited the room");
+        ActivateMyPanel(LobbyPanel.name);
     }
     #endregion
+
 
     #region PHOTON_CALLBACKS
     public override void OnConnected()
@@ -53,6 +101,15 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     {
         Debug.Log(PhotonNetwork.LocalPlayer.NickName + " is connected to photon...");
         ActivateMyPanel(LobbyPanel.name);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log(PhotonNetwork.CurrentRoom.Name + " is created !");
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + "Room Joined");
     }
     #endregion
 
